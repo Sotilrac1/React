5384e01e65df2bad2fed72a116efad7f05de6f15
Carlos Flynn <18237370+Sotilrac1@users.noreply.github.com>
2023-08-05 06:33:24 +0000

    Update 3 files

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -5,10 +5,12 @@ function Expenseitem(props) {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}/>
-      <div className="espense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+        <div className="espense-item__description">
+            <h2>{props.title}</h2>
+              <div className="expense-item__price">
+                ${props.amount}
+              </div>
+        </div>
     </div>
   );
 }
