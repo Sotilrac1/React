821d34ddfafe0fa25222c0d285e3780212a2e023
Carlos Flynn <18237370+Sotilrac1@users.noreply.github.com>
2023-08-05 05:15:14 +0000

    Update Expenseitem.js

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,12 +1,16 @@
-import ".//Expenseitem.css";
+import "./Expenseitem.css";
 
 function Expenseitem() {
+    const expenseDate = new Date(2023,2,28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div>
-        <h2 className="espense-item__description">Insurance</h2>
-        <div className="expense-item__price">249.67</div>
+        <h2 className="espense-item__description">{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
